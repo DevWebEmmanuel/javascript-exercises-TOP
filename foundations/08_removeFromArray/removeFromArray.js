eb7b5226a93ca6bcbfdcd0fcfc3b3294baa46a22
DevWebEmmanuel <devwebemmanuel@gmail.com>
2026-08-13 18:21:59 +0100

    Solution to the given problem

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(table, element) {
+    let index = table.indexOf(element);
+    (index != -1) ?table.splice(index, 1) : table.pop();
+    return table;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+ 
