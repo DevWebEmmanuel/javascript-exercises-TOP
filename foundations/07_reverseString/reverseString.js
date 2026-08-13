b22497d26d052661b7fdef88ba507d81af94adb1
DevWebEmmanuel <devwebemmanuel@gmail.com>
2026-08-13 18:13:14 +0100

    Solution provided to the problem

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let taille = string.length;
+    let result = "";
+    for (let i = taille - 1; i >= 0; i--) {
+        result += string[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
