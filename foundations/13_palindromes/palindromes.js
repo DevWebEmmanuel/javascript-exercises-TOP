fe64e029caff89fb3343d00f7da57ffe8bded8a6
DevWebEmmanuel <devwebemmanuel@gmail.com>
2026-08-20 11:25:21 +0100

    Solution to the given problem

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string.trim();
+    const reverse = string.split("").reverse();
 
+    for (let char of reverse) {
+        if (["?", "!", ".", ","].includes(char)) {
+            let index = reverse.indexOf(char);
+            (index === -1) ? reverse.pop(char): 
+            reverse.splice(index, 1);
+        };
+    };
+    return reverse.join("") === string;
 };
 
 // Do not edit below this line
