f584079b90a5844fc893104c369eb2794b19406e
DevWebEmmanuel <devwebemmanuel@gmail.com>
2026-08-20 10:41:29 +0100

    Solution to the given problem

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  const double = array.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
+  return double;
+;};
 
+const power = function(base, exposant) {
+	return base ** exposant;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  for (let i = 2; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
